24943098c28f79a0137763bddc6fd24b86590d6a
Anisur Rahman <anisur2805@users.noreply.github.com>
2018-10-11 15:35:15 +0600

    Update Load More Post by Ajax

--- load-more-posts.php
+++ load-more-posts.php
@@ -1,54 +1,67 @@
-This first will goes to page template
+This first will goes to page template or index
 
-<?php 
-    $args = array(
-        'post_type' => 'post',
-        'post_status' => 'publish',
-        'posts_per_page' => '2',
-        'paged' => 1,
-    );
-    $my_posts = new WP_Query( $args );
-    if ( $my_posts->have_posts() ) : 
-    ?>
-        <div class="my-posts">
-            <?php while ( $my_posts->have_posts() ) : $my_posts->the_post() ?>
-                <h2><?php the_title() ?></h2>
-                <?php the_excerpt() ?>
-            <?php endwhile ?>
-        </div>
-    <?php endif ?>
-    <div class="loadmore">Load More...</div>
-</div>
+<div class="container">
+				<div class="row">
+					<div class="col-md-8">
+
+						<?php
+
+						$args = array(
+							'post_type' => 'post',
+							'post_status' => 'publish',
+							'posts_per_page' => '2',
+							'paged' => 1,
+						);
+						$my_posts = new WP_Query( $args );
+
+						//var_dump($my_posts->max_num_pages);
+						if ( $my_posts->have_posts() ) : 
+
+							?>
+							<div class="my-posts">
+								<?php while ( $my_posts->have_posts() ) : $my_posts->the_post() ?>
+									<h2><?php the_title() ?></h2>
+									<?php the_excerpt() ?>
+								<?php endwhile ?>
+							</div>
+						<?php endif ?>
+						<div class="loadmore">Load More...</div>
+					</div>
+					<div class="col-md-4"><?php get_sidebar(); ?></div>
+				</div>
+			</div>
 
 <script type="text/javascript">
-var ajaxurl = "<?php echo admin_url( 'admin-ajax.php' ); ?>";
-var page = 2;
-jQuery(function($) {
-    $('body').on('click', '.loadmore', function() {
-        var data = {
-            'action': 'load_posts_by_ajax',
-            'page': page,
-            'security': '<?php echo wp_create_nonce("load_more_posts"); ?>'
-        };
- 
-        $.post(ajaxurl, data, function(response) {
-            $('.my-posts').append(response);
-            page++;
-        });
-    });
-});
+	var ajaxurl = "<?php echo admin_url( 'admin-ajax.php' ); ?>";
+	var page = 2;
+	var maxPage = '<?php echo $my_posts->max_num_pages ?>';
+	jQuery(function($) {
+		$('body').on('click', '.loadmore', function() {
+
+			var data = {
+				'action': 'load_posts_by_ajax',
+				'page': page,
+				'security': '<?php echo wp_create_nonce("load_more_posts"); ?>'
+			};
+			
+			$.post(ajaxurl, data, function(response) {
+				$('.my-posts').append(response);
+				if(page>=maxPage){
+					$('.loadmore').hide();
+				}
+				page++;
+			});
+		});
+	});
 </script>
 
 
 And this goes to functions.php
 <?php
-
+// Load More Post by aJax
 add_action('wp_ajax_load_posts_by_ajax', 'load_posts_by_ajax_callback');
 add_action('wp_ajax_nopriv_load_posts_by_ajax', 'load_posts_by_ajax_callback');
-
-
  
-
 function load_posts_by_ajax_callback() {
     check_ajax_referer('load_more_posts', 'security');
     $paged = $_POST['page'];
@@ -70,3 +83,4 @@ function load_posts_by_ajax_callback() {
  
     wp_die();
 }
+?>
